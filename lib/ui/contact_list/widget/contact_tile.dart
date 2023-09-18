6c6ed0f848746b779934c19240a45223dda98062
yordanos <yordanosfikadu2017@gmail.com>
2023-09-18 13:03:15 +0300

    create edot page

--- lib/ui/contact_list/widget/contact_tile.dart
+++ lib/ui/contact_list/widget/contact_tile.dart
@@ -1,4 +1,4 @@
-import 'package:contact_app/data/contact.dart';
+import 'package:contact_app/ui/contact/contact_edit_page.dart';
 import 'package:contact_app/ui/model/contacts_model.dart';
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
@@ -8,21 +8,23 @@ class ContactTile extends StatelessWidget {
   final int contactIndex;
   @override
   Widget build(BuildContext context) {
-    return ScopedModelDescendant<ContactsModel>(
-      builder: (context, child, model) {
-        final displayContact = model.contacts[contactIndex];
-        return ListTile(
-            title: Text(displayContact.name),
-            subtitle: Text(displayContact.email),
-            trailing: IconButton(
-              onPressed: () {
-                model.changeFavoriteStatus(contactIndex);
-              },
-              icon: Icon(
-                  displayContact.isFavorite ? Icons.star : Icons.star_border,
-                  color:
-                      displayContact.isFavorite ? Colors.amber : Colors.grey),
-            ));
+    final model = ScopedModel.of<ContactsModel>(context);
+    final displayContact = model.contacts[contactIndex];
+    return ListTile(
+      title: Text(displayContact.name),
+      subtitle: Text(displayContact.email),
+      trailing: IconButton(
+        onPressed: () {
+          model.changeFavoriteStatus(contactIndex);
+        },
+        icon: Icon(displayContact.isFavorite ? Icons.star : Icons.star_border,
+            color: displayContact.isFavorite ? Colors.amber : Colors.grey),
+      ),
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(
+            builder: (_) => ContactEditPage(
+                editedContact: displayContact,
+                editedContactIndex: contactIndex)));
       },
     );
   }
